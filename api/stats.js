d6d09d0a3d51ed74684203432ae71cbd448924d2
izzypositivetech-001 <izzypositivetech@gmail.com>
2025-11-14 16:51:21 +0100

    Fix: Correct OPTIONS preflight response handling for CORS

--- api/stats.js
+++ api/stats.js
@@ -6,9 +6,11 @@ export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Max-Age', '86400');
 
   if (req.method === 'OPTIONS') {
-    return res.status(200).end();
+    res.status(200);
+    return res.end();
   }
 
   if (req.method !== 'GET') {
